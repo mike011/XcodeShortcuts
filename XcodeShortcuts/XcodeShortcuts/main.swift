ba6ef70455de0c6d55033a533ac3f5798e8f4af9
mike011 <mike_ch_1@hotmail.com>
2021-01-09 17:12:58 -0500

    more shortcuts

--- XcodeShortcuts/XcodeShortcuts/main.swift
+++ XcodeShortcuts/XcodeShortcuts/main.swift
@@ -17,6 +17,17 @@ private let shortcuts = [Shortcut(description:"Find", keys: "⌘ + f"),
                          Shortcut(description:"Show/Hide Debug Area", keys: "⇧ + ⌘ + y"),
                          Shortcut(description:"Show/Hide Navigator", keys: "⌘ + 0"),
                          Shortcut(description:"Show/Hide Inspector", keys: "⌥ + ⌘ + 0"),
-                         Shortcut(description:"Fix All Issues", keys: "⌃ + ⌥ + ⌘ + f"),]
+                         Shortcut(description:"Show/Hide Debug Area", keys: "⇧ + ⌘ + y"),
+                         Shortcut(description:"Show/Hide Navigator", keys: "⌘ + 0"),
+                         Shortcut(description:"Show/Hide Inspector", keys: "⌥ + ⌘ + 0"),
+                         Shortcut(description:"Add/Remove Split Windows", keys: "⇧ + ⌃ + ⌘ + ↩"),
+                         Shortcut(description:"Jump to Method", keys: "⌃ + 6"),
+                         Shortcut(description:"Go to Line", keys: "⌘ + l"),
+                         Shortcut(description:"Switch Tabs", keys: "⇧ + ⌘ + }"),
+                         Shortcut(description:"Open Call Hierarchy", keys: "⇧ + ⌃ + ⌘ + h"),
+                         Shortcut(description:"Fix All Issues", keys: "⌃ + ⌥ + ⌘ + f"),
+                         Shortcut(description:"Extract to method", keys: "⇧ + ⌥ + e"),
+                         Shortcut(description:"Rename", keys: "⌥ r"),
+]
 var manager = ShortcutManager(shortcuts: shortcuts)
 manager.go()
